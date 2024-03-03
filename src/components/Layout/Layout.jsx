96fd8f22337d614d26751ea46574ed2bfa097c76
Paweł Brzozowski <pawel@brzoza.net>
2024-03-03 22:12:40 +0100

    Layout update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
-import { AppBar } from 'components/AppBar/AppBar';
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { AppBar } from "components/AppBar/AppBar";
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
