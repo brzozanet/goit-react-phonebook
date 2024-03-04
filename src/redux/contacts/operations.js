dcc8aef9949da90c5b0e99a78f2063649fa1de3b
Paweł Brzozowski <pawel@brzoza.net>
2024-03-04 19:34:52 +0100

    fetchContacts fix

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://64b5425ff3dbab5a95c708e0.mockapi.io";
+// axios.defaults.baseURL = "https://64b5425ff3dbab5a95c708e0.mockapi.io";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
