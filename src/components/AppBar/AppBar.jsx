db45773395721b28a4db1f6f710c4df30aa2b04c
Paweł Brzozowski <pawel@brzoza.net>
2024-03-04 22:44:05 +0100

    AppBar CSS

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,15 +3,13 @@ import { useAuth } from "hooks/useAuth";
 import { Navigation } from "../Navigation/Navigation";
 import { UserMenu } from "../UserMenu/UserMenu";
 
-const styles = ["flex", "justify-between", "border-b-2", "pb-3", "mb-2"].join(
-  " "
-);
+const css = ["flex", "justify-between", "border-b-2", "pb-3", "mb-2"].join(" ");
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <header className={styles}>
+    <header className={css}>
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
