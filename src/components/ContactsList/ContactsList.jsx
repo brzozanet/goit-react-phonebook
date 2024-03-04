23889398aaa421b42deadf20000d83284159cc23
Paweł Brzozowski <pawel@brzoza.net>
2024-03-04 19:56:07 +0100

    Delete button layout

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { deleteContact } from "redux/contacts/operations";
 import { selectVisibleContacts } from "redux/contacts/selectors";
 import css from "./ContactsList.module.css";
+import { Button } from "@mui/material";
 
 export const ContactsList = () => {
   const contacts = useSelector(selectVisibleContacts);
@@ -15,14 +16,9 @@ export const ContactsList = () => {
         <li key={contact.id} className={css.contactsItem}>
           <div>
             {contact.name}: {contact.number}
-            <button
-              type="submit"
-              value={contact.id}
-              className={css.button}
-              onClick={() => handleDelete(contact.id)}
-            >
-              Delete
-            </button>
+            <Button type="submit" onClick={() => handleDelete(contact.id)}>
+              Delete {contact.name}
+            </Button>
           </div>
         </li>
       ))}
